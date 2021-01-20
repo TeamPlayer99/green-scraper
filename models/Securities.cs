0a52b446a39231de60543771388fe4739e5c3bb4
Abdulgafar Jagun <jagun.abdulgafar@live.com>
2021-01-20 14:33:14 +0100

    Initial commit

---
+++ models/Securities.cs
@@ -0,0 +1,20 @@
+namespace scrapy.models
+{
+    public class Security
+    {
+        public string AuctiionDate { get; set; }
+        public string SecurityType { get; set; }
+        public string Tenor { get; set; }
+        public string AuctionNumber { get; set; }
+        public string Auction { get; set; }
+        public string MaturityDate { get; set; }
+        public string TotalSubscription { get; set; }
+        public string TotalSuccessful { get; set; }
+        public string RangeBid { get; set; }
+        public string SuccessfulBidRate { get; set; }
+        public string Description { get; set; }
+        public string Rate { get; set; }
+        public string TrueYield { get; set; }
+        public string AmountOffered { get; set; }
+    }
+}
